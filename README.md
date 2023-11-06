7567d889bf939e1090298ec454454e25199abf29
dpet23 <dpet23@users.noreply.github.com>
2023-11-06 13:59:07 +1100

    Updated build scripts to use Rollup for end-to-end building/packaging
    Rollup now compiles TS -> JS (using a plugin to call `tsc`), and then bundles the JS into a single file.
    This is done in 1 command, and removes the temporary "build" folder.
    A custom plugin was needed to remove the external imports from the generated bundle.

--- README.md
+++ README.md
@@ -108,9 +108,8 @@ Use the Node Package Manager (`npm`) to manage the dependencies and run commands
 | `npm run lint` | Run [ESLint](https://www.npmjs.com/package/eslint) over the `src` files (includes [Prettier](https://www.npmjs.com/package/eslint-plugin-prettier) and [SonarJS](https://www.npmjs.com/package/eslint-plugin-sonarjs)) |
 | `npm run lint:fix` | Run the lint check and automatically fix problems |
 | `npm version [major\|minor\|patch]` | Version bump _(in `package.json`)_ |
-| `npm run build` | Build the library for production to the `build` folder |
-| `npm run package` | Bundle the production packages into a single ES Module, in the `dist` folder |
-| `npm run dist` | Wrapper for `build && package`<br>Build the library for production and bundle into a single ES Module |
+| `npm run build` | Compile the TypeScript code into JavaScript (for development only) |
+| `npm run dist` | Build the library for production as a single ES Module, in the `dist` folder |
 
 `npx` is used to execute npm packages that haven't been installed globally.
 `nvm` can be used to manage the current version of Node.
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,61 @@
-export default {
-    input: 'build/main.js',
-    output: {
-        file: 'dist/leaflet-map.esm.js',
-        format: 'esm',
+/**
+ * Rollup configuration for bundling production packages.
+ */
+
+import fs from 'fs';
+
+import typescript from 'rollup-plugin-typescript2';
+
+const OUTPUT_DIR = 'dist';
+
+/**
+ * Rollup plugin to remove external imports from the generated bundle.
+ */
+const removeExternalExports = () => ({
+    name: 'Remove external imports',
+    writeBundle(options, bundle) {
+        for (const [fileName, chunkOrAsset] of Object.entries(bundle)) {
+            if (!options.file.endsWith(fileName)) {
+                continue;
+            }
+            const content = chunkOrAsset.code || chunkOrAsset.source;
+            fs.writeFile(
+                options.file,
+                content.replace(/import.*/, ''),
+                err => {
+                    if (err) {
+                        throw err
+                    }
+                }
+            );
+        }
     },
-    external: ['leaflet'],
+});
+
+export default {
+    input: 'src/main.ts',
+
+    plugins: [
+        // Call the TypeScript compiler.
+        // Loads compilerOptions from tsconfig.json.
+        typescript({
+            abortOnError: true,
+        }),
+    ],
+
+    // Don't package external dependencies.
+    external: [
+        'leaflet',
+    ],
+
+    output: {
+        file: `${OUTPUT_DIR}/leaflet-map.esm.js`,
+        format: 'esm',
+        validate: true,
+
+        plugins: [
+            // Remove external imports from the generated bundle.
+            removeExternalExports(),
+        ],
+    },
 };
