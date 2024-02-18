eb7825e82333f931651e0b5b74890ccced5531e0
dpet23 <dpet23@users.noreply.github.com>
2024-02-18 12:07:06 +1100

    Set up browser automation testing via the Selenium library

--- test/system.test.ts
+++ test/system.test.ts
@@ -1,3 +1,6 @@
+import { Builder, ThenableWebDriver } from 'selenium-webdriver';
+import Chrome from 'selenium-webdriver/chrome';
+
 /**
  * Sample function to test.
  */
@@ -7,4 +10,27 @@ function sum(a, b) {
 
 it('should correctly perform addition', () => {
     expect(sum(1, 2)).toBe(3);
+});
+
+describe('Sandbox', () => {
+    jest.setTimeout(1 * 60 * 1000); // 1 minute
+
+    let driver: ThenableWebDriver;
+
+    beforeAll(() => {
+        const options = new Chrome.Options();
+        options.addArguments('--headless=new');
+
+        // eslint-disable-next-line newline-per-chained-call
+        driver = new Builder().forBrowser('chrome').setChromeOptions(options).build();
+    });
+
+    afterAll(async () => {
+        await driver.quit();
+    });
+
+    it('should load Google', async () => {
+        await driver.get('https://www.google.com/');
+        expect(await driver.getTitle()).toEqual('Google');
+    });
 });
