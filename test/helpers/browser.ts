2a7d17867c07c4722ec8357f083aee5cc3861f11
dpet23 <dpet23@users.noreply.github.com>
2024-03-03 14:18:59 +1100

    Created tests for the custom Leaflet components

--- test/helpers/browser.ts
+++ test/helpers/browser.ts
@@ -1,6 +1,6 @@
 /* Set up a webdriver. */
 
-import { Browser, Builder, ThenableWebDriver, WebDriver } from 'selenium-webdriver';
+import { Browser, Builder, ThenableWebDriver, WebDriver, WebElement } from 'selenium-webdriver';
 import Chrome from 'selenium-webdriver/chrome';
 import Edge from 'selenium-webdriver/edge';
 import Firefox from 'selenium-webdriver/firefox';
@@ -16,11 +16,75 @@ export let browser: ThenableWebDriver & ExtendedWebDriver;
 class ExtendedWebDriver extends WebDriver {
     /**
      * Fetch the error log entries shown in the browser's console.
+     * NOTE: not supported on Firefox.
      */
     async getErrorLogs(): Promise<Entry[]> {
         return (await this.manage().logs().get('browser')).filter(
-            entry => entry.level <= Level.SEVERE && !entry.message.includes('favicon.ico'),
+            entry =>
+                // Errors only
+                entry.level <= Level.SEVERE &&
+                // Browsers try to fetch the favicon automatically, ignore this error
+                !entry.message.includes('favicon.ico') &&
+                // Ignore any errors from fetching the background map tiles
+                !entry.message.includes('google.com'),
         );
+    }
+
+    /**
+     * Check if the browser is currently in fullscreen mode.
+     */
+    async isFullscreen(): Promise<boolean> {
+        // The `fullscreenElement` property returns the Element that is currently being presented in fullscreen mode,
+        // or null if fullscreen mode is not in use.
+        return (await this.executeScript('return document.fullscreenElement')) !== null;
+    }
+
+    /**
+     * Move the mouse over an element and click on it.
+     *
+     * @param element - The element to interact with.
+     * @param options - Customise the actions to take.
+     * @param options.pauseAfterMove - Milliseconds to wait after moving the mouse.
+     * @param options.pauseAfterClick - Milliseconds to wait after clicking on the element.
+     */
+    async moveToAndClick(
+        element: WebElement,
+        { pauseAfterMove, pauseAfterClick }: { pauseAfterMove?: number; pauseAfterClick?: number } = {},
+    ): Promise<void> {
+        let actions = this.actions({ async: true });
+
+        actions = actions.move({ origin: element });
+        if (pauseAfterMove) {
+            actions = actions.pause(pauseAfterMove);
+        }
+
+        actions = actions.click();
+        if (pauseAfterClick) {
+            actions = actions.pause(pauseAfterClick);
+        }
+
+        await actions.perform();
+    }
+
+    /**
+     * Find the zoom bar that's currently highlighted, and extract the zoom level from its ID.
+     *
+     * @return The map's current zoom level.
+     */
+    async getLeafletMapZoomLevel(): Promise<number> {
+        const selectedZoomBarId = await this.findElement({ className: 'zoom-bar-selected' }).getAttribute('id');
+        return Number(selectedZoomBarId.substring(selectedZoomBarId.lastIndexOf('-') + 1));
+    }
+
+    /**
+     * Zoom the map to a certain level, by clicking on the appropriate zoom bar.
+     *
+     * @return The zoom bar element.
+     */
+    async zoomLeafletMapTo(level: number): Promise<WebElement> {
+        const zoomBar = await this.findElement({ id: `zoom-bar-${level}` });
+        await this.moveToAndClick(zoomBar, { pauseAfterClick: 500 });
+        return zoomBar;
     }
 }
 
@@ -32,9 +96,11 @@ export const startBrowser = async () => {
     const edgeOptions = new Edge.Options();
     const firefoxOptions = new Firefox.Options();
 
-    // chromeOptions.addArguments('--headless=new');
-    // edgeOptions.addArguments('--headless=new');
-    // firefoxOptions.addArguments('--headless');
+    if (process.env.SELENIUM_HEADLESS?.toLowerCase() === 'true') {
+        chromeOptions.addArguments('--headless=new');
+        edgeOptions.addArguments('--headless=new');
+        firefoxOptions.addArguments('--headless');
+    }
 
     // Configure the WebDriver instance for all supported browsers.
     // Any options that are irrelevant to the selected browser are automatically ignored.
